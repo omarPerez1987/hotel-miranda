12f180868f3dda4a33e5e5205a2f75e6b70ad9d4
omarPerez1987 <omar.perez.dev@gmail.com>
2023-11-09 20:38:40 +0100

    style: añado estilos a homepage (intro-video y facilities)

--- app.js
+++ app.js
@@ -14,3 +14,23 @@ const swiper = new Swiper("#mySwiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+
+const swiperFacilities = new Swiper("#swiper-facilities",{
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el:".swiper-pagination",
+  },
+  breakpoints:{
+    1000:{
+      slidesPerView: 3,
+      
+      grid:{
+        fill:"row",
+        rows:2,
+      }
+    }
+  }
+})
