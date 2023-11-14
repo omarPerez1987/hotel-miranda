da7201edcdbcce9982bcf046dd7837af0a706fd0
omarPerez1987 <omar.perez.dev@gmail.com>
2023-11-14 21:14:34 +0100

    feat: añado estilos y funcionalidad  a offers

--- scripts/swipers.js
+++ scripts/swipers.js
@@ -15,69 +15,64 @@ const swiper = new Swiper("#mySwiper", {
   keyboard: true,
 });
 
-
-const swiperFacilities = new Swiper("#swiper-facilities",{
+const swiperFacilities = new Swiper("#swiper-facilities", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   pagination: {
-    el:".swiper-pagination",
+    el: ".swiper-pagination",
   },
-  breakpoints:{
-    1000:{
+  breakpoints: {
+    1000: {
       slidesPerView: 3,
-      
-      grid:{
-        fill:"row",
-        rows:2,
-      }
-    }
-  }
-})
 
-const swiperMenu = new Swiper("#swiperMenu",{
+      grid: {
+        fill: "row",
+        rows: 2,
+      },
+    },
+  },
+});
+
+const swiperMenu = new Swiper("#swiperMenu", {
   slidesPerView: 1,
   spaceBetween: 16,
   loop: true,
-  grid:{
-    fill:"row",
-    rows:3,
+  grid: {
+    fill: "row",
+    rows: 3,
   },
   navigation: {
-    prevEl: '.swiper-button-prev',
-    nextEl: '.swiper-button-next',
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
   },
-  breakpoints:{
-    1000:{
+  breakpoints: {
+    1000: {
       slidesPerView: 2,
 
-      grid:{
-        fill:"row",
-        rows:3,
-      }
-    }
+      grid: {
+        fill: "row",
+        rows: 3,
+      },
+    },
   },
-})
+});
 
-
-const swiperMenuPhotos = new Swiper ("#menu-photos",{
-  slidesPerView:1,
+const swiperMenuPhotos = new Swiper("#menu-photos", {
+  slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
 
-  pagination:{
-    el:".menu-photos__pagination"
+  pagination: {
+    el: ".menu-photos__pagination",
   },
-  breakpoints:{
-    1000:{
+  breakpoints: {
+    1000: {
       slidesPerView: 3,
       spaceBetween: 30,
-    }
+    },
   },
-})
-
-
-
+});
 
 const swiperAboutCounter = new Swiper("#swiper-about", {
   slidesPerView: 1,
@@ -93,4 +88,25 @@ const swiperAboutCounter = new Swiper("#swiper-about", {
       spaceBetween: 30,
     },
   },
+});
+
+const swiperRelatedRooms = new Swiper("#swiperRelatedRooms", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    prevEl: ".related-rooms__scroll__button-prev",
+    nextEl: ".related-rooms__scroll__button-next",
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      grid: {
+        fill: "row",
+        rows: 1,
+      },
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
 });
