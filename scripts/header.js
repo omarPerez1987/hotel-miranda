e129beb9f9f6291d61342e90082ea34980543836
omarPerez1987 <omar.perez.dev@gmail.com>
2023-11-19 13:12:26 +0100

    styles: añado rectificaciones en about

---
+++ scripts/header.js
@@ -0,0 +1,19 @@
+// const showHeader = document.querySelector(".header-box__header");
+
+// document.addEventListener('mouseover', (event) => {
+//   const scrollY = window.scrollY;
+//   const clientY = event.clientY;
+
+//   if (window.innerWidth > 1000) {
+//     if (scrollY > 0 && clientY < 100) {
+//       showHeader.style.position = 'fixed';
+//       showHeader.style.opacity = '1';
+//     } else {
+//       showHeader.style.position = 'static';
+//       showHeader.style.opacity = '0';
+//     }
+//   } else {
+//     showHeader.style.position = 'fixed';
+//     showHeader.style.opacity = '1';
+//   }
+// });
