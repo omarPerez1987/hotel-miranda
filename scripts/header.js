0f90452ee6fd4bb7f9ce5e01faad4ac47b066cb8
omarPerez1987 <omar.perez.dev@gmail.com>
2023-11-19 20:36:09 +0100

    feat: añado funcionalidad al header

--- scripts/header.js
+++ scripts/header.js
@@ -1,19 +1,24 @@
-// const showHeader = document.querySelector(".header-box__header");
+const showHeader = document.querySelector(".header-box__header");
 
-// document.addEventListener('mouseover', (event) => {
-//   const scrollY = window.scrollY;
-//   const clientY = event.clientY;
+function handleScroll() {
+  const scrollHeight = window.scrollY;
+  const clientY = event.clientY;
 
-//   if (window.innerWidth > 1000) {
-//     if (scrollY > 0 && clientY < 100) {
-//       showHeader.style.position = 'fixed';
-//       showHeader.style.opacity = '1';
-//     } else {
-//       showHeader.style.position = 'static';
-//       showHeader.style.opacity = '0';
-//     }
-//   } else {
-//     showHeader.style.position = 'fixed';
-//     showHeader.style.opacity = '1';
-//   }
-// });
+  if (window.innerWidth <= 1000) {
+    return;
+  }
+
+  if (scrollHeight < 500) {
+    showHeader.style.position = "static";
+    showHeader.style.transform = "translateY(0)";
+  } else if (scrollHeight > 500 && clientY < 100) {
+    showHeader.style.position = "fixed";
+    showHeader.style.transform = "translateY(0)";
+  } else if (scrollHeight > 500 && clientY > 100) {
+    showHeader.style.position = "fixed";
+    showHeader.style.transform = "translateY(-200%)";
+  }
+}
+
+document.addEventListener("mouseover", handleScroll);
+window.addEventListener("scroll", handleScroll);
